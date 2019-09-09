42a82b30c83fd318546dbc0070b17426eea3ae82
m1yag1 <mike.arbelaez@rice.edu>
2019-09-09 12:20:10 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+requires = []
+packages = ["archive_client"]
+
+requires.extend([
+    "requests==2.22.0",
+    "Beautifulsoup4==4.8.0"
+])
+
+setuptools.setup(
+    name="archive-api-client",
+    version="0.0.1",
+    description=("Python wrapper for the CNX Archive API"
+                 "(http://archive.cnx.org)"),
+    long_description=open("README.rst").read(),
+    license="AGPLv3",
+    author="m1yag1",
+    author_email="mike.arbelaez@rice.edu",
+    url="",
+    packages=packages,
+    package_dir={"": "src"},
+    install_requires=requires,
+    classifiers=[
+    ],
+    extras_require={
+    },
+)
